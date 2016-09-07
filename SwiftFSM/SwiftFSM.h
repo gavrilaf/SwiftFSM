1d232ddc4121513b2a27a358fae96b774d49842a
Eugen Fedchenko <eugen.fedchenko@bluechipsys.com>
2016-09-07 14:48:47 +0300

    Initial Commit

---
+++ SwiftFSM/SwiftFSM.h
@@ -0,0 +1,19 @@
+//
+//  SwiftFSM.h
+//  SwiftFSM
+//
+//  Created by Eugen Fedchenko on 9/7/16.
+//  Copyright © 2016 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftFSM.
+FOUNDATION_EXPORT double SwiftFSMVersionNumber;
+
+//! Project version string for SwiftFSM.
+FOUNDATION_EXPORT const unsigned char SwiftFSMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFSM/PublicHeader.h>
+
+
